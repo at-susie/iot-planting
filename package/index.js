a8541a61261a31e51c4dee329b54acab078c3c0a
atsushiienaka <atsuboy@gmail.com>
2020-07-23 08:56:57 +0200

    Fixed calculation method for soil moisture and temp
    Also fixed a layout issue for notification message in moisture, which comes from bootstrap.

--- package/index.js
+++ package/index.js
@@ -16,7 +16,6 @@ app.use(express.static(__dirname + '/public'));
 // you need a  body parser:
 app.use(bodyParser.urlencoded({ extended: false })); // for application/x-www-form-urlencoded
 
-
 // this is the POST handler:
 app.all('/*', function (request, response) {
 	console.log('Got a ' + request.method + ' request');
@@ -26,24 +25,28 @@ app.all('/*', function (request, response) {
 	// console.log(request.headers);
 
 	// Moisture (Need your own calibration)
+	const valueInAir = 693;
+	const valueInWater = 250;
+	const thresholdMoisture = valueInAir - valueInWater;
 	const moistureOrigin = request.body.moistureLevel;
-	const moistureDisplayed = Math.round((692 - moistureOrigin) / 440 * 100);
-	
+	const moisturecalculated = Math.round((valueInAir - moistureOrigin) / thresholdMoisture * 100);
+	const moistureDisplayed = (moisturecalculated >= 100) ? 100 : moisturecalculated;
+
 	// Light (Need your own calibration)
 	const lightOrigin = request.body.lightLevel;
 	const lightDisplayed = 1200 - lightOrigin;
 
 	// Temp (Need your own calibration)
 	const tempOrigin = request.body.tempLevel;
-	const tempDisplayed = Math.round(tempOrigin - 210);
+	const tempDisplayed = Math.round(tempOrigin * 0.48828125) - 95;
 
 	if (request.method == 'GET') {
 		console.log(request.query);
 
 	} else {
-		console.log("Moisture is:" + request.body.moistureLevel);
-		console.log("Light is:" + request.body.lightLevel);
-		console.log("Temp is:" + request.body.tempLevel);
+		console.log("Moisture is:" + moistureOrigin);
+		console.log("Light is:" + lightOrigin);
+		console.log("Temp is:" + tempOrigin);
 		io.emit('moistureLevel', moistureDisplayed);
 		io.emit('lightLevel', lightDisplayed);
 		io.emit('tempLevel', tempDisplayed);
@@ -54,7 +57,6 @@ app.all('/*', function (request, response) {
 	response.end();
 
 });
-
 
 // start the server:
 //var server = app.listen(8080, serverStart);
